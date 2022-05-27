3ae7f9fe1b36fde29d25991014bff8486c4925ac
187PM14027 Nguyen Hoang Trung K24T <hoangtrung7762@gmail.com>
2022-05-27 13:13:43 +0700

    Refining Header 2 - Scrollbar, warnning Tippy, favion

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  return <h2>Home</h2>;
 }
 
-export default Home
+export default Home;
--- src/component/Layout/components/Search/index.js
+++ src/component/Layout/components/Search/index.js
@@ -58,47 +58,51 @@ function Search() {
   };
 
   return (
-    <HeadlessTippy
-      interactive
-      visible={showResult && searchResult.length > 0}
-      onClickOutside={handleHideResult}
-      render={(attrs) => (
-        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-          <PopperWrapper>
-            <h4 className={cx('search-title')}>Accounts</h4>
-            {searchResult.map((result) => (
-              <AccountItems key={result.id} data={result} />
-            ))}
-          </PopperWrapper>
-        </div>
-      )}
-    >
-      <div className={cx('search')}>
-        <input
-          value={searchValue}
-          ref={inputRef}
-          placeholder="Search accounts and videos"
-          spellCheck={false}
-          onChange={handleChange}
-          onFocus={() => setShowResult(true)}
-        />
-        {!!searchValue && !loading && (
-          <button className={cx('clear')} onClick={handleClear}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
+    // Using a wrapper <div> tag around the reference element solves this
+    // by creating a new parentNode context.
+    <div>
+      <HeadlessTippy
+        interactive
+        visible={showResult && searchResult.length > 0}
+        onClickOutside={handleHideResult}
+        render={(attrs) => (
+          <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx('search-title')}>Accounts</h4>
+              {searchResult.map((result) => (
+                <AccountItems key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
         )}
-        {loading && (
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-        )}
+      >
+        <div className={cx('search')}>
+          <input
+            value={searchValue}
+            ref={inputRef}
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+            onChange={handleChange}
+            onFocus={() => setShowResult(true)}
+          />
+          {!!searchValue && !loading && (
+            <button className={cx('clear')} onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+          {loading && (
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          )}
 
-        <button
-          className={cx('search-btn')}
-          onMouseDown={(e) => e.preventDefault()}
-        >
-          <SearchIcon />
-        </button>
-      </div>
-    </HeadlessTippy>
+          <button
+            className={cx('search-btn')}
+            onMouseDown={(e) => e.preventDefault()} // prevent event focus when click icon search
+          >
+            <SearchIcon />
+          </button>
+        </div>
+      </HeadlessTippy>
+    </div>
   );
 }
 
